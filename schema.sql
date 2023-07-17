b8d6230957aaccaa8b899d79a9b30e3e6964968c
Enobong Isong <enobongitiaba988@gmail.com>
2023-07-17 11:31:08 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
+USE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+  id INTEGER,
+  name VARCHAR(255),
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL(5,2)
 );
+
