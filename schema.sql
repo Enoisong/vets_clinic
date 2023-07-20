aa2dd3d31942ebbffe99e643f6e3c82781cb7659
Enoisong <enobongitiaba988@gmail.com>
2023-07-19 22:21:32 -0700

    add table owners and species

--- schema.sql
+++ schema.sql
@@ -12,3 +12,28 @@ CREATE TABLE animals (
 -- Add the species column to the animals table
 ALTER TABLE animals 
 ADD species VARCHAR(50);
+
+-- Create owners table in Vet_Clinic Database
+CREATE TABLE owners (
+    id PRIMARY KEY SERIAL,
+    full_name VARCHAR(100),
+    age INTEGER    
+);
+
+-- Create species Table in vet_clinic Database
+CREATE TABLE species (
+    id PRIMARY KEY SERIAL,
+    name VARCHAR(100)
+);
+
+-- Remove the column species
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add column species_id as a foreign key referencing the species table
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+
+-- Add column owner_id as a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id);
